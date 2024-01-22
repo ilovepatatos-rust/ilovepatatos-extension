58d00da42523b3a29bff491975c64436b1111d25
Ilovepatatos <gabriel@khome.org>
2024-01-22 17:40:44 -0500

    Init

---
+++ src/IlovepatatosExt/IlovepatatosExt.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Oxide.Core;
+using Oxide.Core.Extensions;
+
+namespace Oxide.Ext.IlovepatatosExt
+{
+    [UsedImplicitly]
+    public class IlovepatatosExt : Extension
+    {
+        private static readonly VersionNumber s_ExtensionVersion = new(1, 0, 0);
+
+        public override string Name => "IlovepatatosExt";
+        public override string Author => "Ilovepatatos";
+        public override VersionNumber Version => s_ExtensionVersion;
+
+        public override bool SupportsReloading => true;
+
+        public IlovepatatosExt(ExtensionManager manager) : base(manager) { }
+    }
+}
