fbbd82c5f3246284605c42278da5f3ffc3bd897f
Ilovepatatos <gabriel@khome.org>
2024-01-27 15:53:15 -0500

    Create ColorUtility.cs

---
+++ src/IlovepatatosExt/Utility/ColorUtility.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class ColorUtility
+{
+    public static Color ParseHexString(string hex)
+    {
+        UnityEngine.ColorUtility.TryParseHtmlString(hex, out Color color);
+        return color;
+    }
+}
