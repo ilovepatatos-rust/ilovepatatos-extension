13b5a58b1a5aba30abb1b36a6926d5f8990a30a3
Ilovepatatos <gabriel@khome.org>
2024-01-27 15:53:26 -0500

    Create ColorEx.cs

---
+++ src/IlovepatatosExt/Extensions/ColorEx.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class ColorEx
+{
+    public static Vector3 ToVector3(this Color self)
+    {
+        return new Vector3(self.r, self.g, self.b);
+    }
+
+    public static string ToCuiColor(this Color self, float alpha = 1)
+    {
+        return $"{self.r} {self.g} {self.b} {alpha}";
+    }
+}
