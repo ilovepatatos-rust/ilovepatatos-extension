f2c55797c4ffe7313da15121b1a8d2028396fbe4
Ilovepatatos <gabriel@khome.org>
2024-01-27 15:53:40 -0500

    Create MapMarkerGenericRadiusEx.cs

---
+++ src/IlovepatatosExt/Extensions/MapMarkerGenericRadiusEx.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class MapMarkerGenericRadiusEx
+{
+    public static void SendUpdateToPlayer(this MapMarkerGenericRadius marker, BasePlayer player)
+    {
+        float a = marker.color1.a;
+        float alpha = marker.alpha;
+        float radius = marker.radius;
+
+        var inner = marker.color1.ToVector3();
+        var outer = marker.color2.ToVector3();
+
+        marker.ClientRPCPlayer(null, player, "MarkerUpdate", inner, a, outer, alpha, radius);
+    }
+}
