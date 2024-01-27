bd8a3cb3a042f3c1bab059e731b44ffe8da7d607
Ilovepatatos <gabriel@khome.org>
2024-01-27 16:42:01 -0500

    Update CuiHelperUtility.cs

--- src/IlovepatatosExt/Utility/CuiHelperUtility.cs
+++ src/IlovepatatosExt/Utility/CuiHelperUtility.cs
@@ -1,28 +1,10 @@
 ﻿using JetBrains.Annotations;
-using Network;
 
 namespace Oxide.Ext.IlovepatatosExt;
 
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public static class CuiHelperUtility
 {
-    private static void BroadcastAction(List<Connection> connections, string action, string arg)
-    {
-        if (connections.Count > 0)
-            CommunityEntity.ServerInstance.ClientRPCEx(new SendInfo(connections), null, action, arg);
-    }
-
-    public static void AddUi(IEnumerable<BasePlayer> players, string json)
-    {
-        var connections = players.GetOnlineConnectionsPooled();
-        BroadcastAction(connections, json, "AddUI");
-        PoolUtility.Free(ref connections);
-    }
-
-    public static void DestroyUi(IEnumerable<BasePlayer> players, string name)
-    {
-        var connections = players.GetOnlineConnectionsPooled();
-        BroadcastAction(connections, name, "DestroyUI");
-        PoolUtility.Free(ref connections);
-    }
+    public static void AddUi(IEnumerable<BasePlayer> players, string json) => players.AddUi(json);
+    public static void DestroyUi(IEnumerable<BasePlayer> players, string name) => players.DestroyUi(name);
 }
