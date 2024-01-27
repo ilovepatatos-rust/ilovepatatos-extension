37fd60a5e05a93bf21a13bb971ea39d4eb80223a
Ilovepatatos <gabriel@khome.org>
2024-01-27 16:42:32 -0500

    Update MapUtility
    + Added CreateSpawnPointsOnTopology()

---
+++ src/IlovepatatosExt/Utility/CoroutineUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class CoroutineUtility
+{
+    public static Coroutine StartCoroutine(IEnumerator enumerator)
+    {
+        return ServerMgr.Instance.StartCoroutine(enumerator);
+    }
+
+    public static void StopCoroutine(Coroutine coroutine)
+    {
+        if (coroutine != null)
+            ServerMgr.Instance.StopCoroutine(coroutine);
+    }
+}
--- src/IlovepatatosExt/Utility/MapUtility.cs
+++ src/IlovepatatosExt/Utility/MapUtility.cs
@@ -1,5 +1,10 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using JetBrains.Annotations;
+using Oxide.Core;
 using UnityEngine;
+using Parallel = UnityEngine.Parallel;
 
 namespace Oxide.Ext.IlovepatatosExt;
 
@@ -13,6 +18,9 @@ public static class MapUtility
                                            (1 << (int)Rust.Layer.Construction) | (1 << (int)Rust.Layer.Terrain) |
                                            (1 << (int)Rust.Layer.Transparent) | (1 << (int)Rust.Layer.Vehicle_Large) |
                                            (1 << (int)Rust.Layer.Tree);
+
+    private const float MAX_DELAY = 30f;
+    private static int s_Count;
 
     public static string ToGrid(Vector3 pos)
     {
@@ -44,5 +52,87 @@ public static class MapUtility
     {
         int layer = TerrainMeta.TopologyMap.GetTopology(pos);
         return layer.ContainsTopology(topology);
+    }
+
+    /// <summary>
+    /// Create spawn points on a topology inside a rect at a given position and size.
+    /// </summary>
+    /// <returns>Returns a list of locations and the time it took in milliseconds.</returns>
+    public static void CreateSpawnPointsOnTopology(Vector3 center, float lenght,
+        TerrainTopology.Enum targetTopology, int maxAmount, Action<List<Vector3>, long> onComplete)
+    {
+        Rect rect = CreateRectAt(center, lenght);
+        var watch = Stopwatch.StartNew();
+
+        IEnumerator task = GetAllAvailableSpawnPointsWithin(rect, targetTopology, locations =>
+        {
+            List<Vector3> list = locations.ToPooledList();
+
+            uint seed = (uint)new System.Random().Next();
+            list.Shuffle(seed);
+
+            var values = new List<Vector3>(maxAmount);
+
+            foreach (Vector3 x in list.Take(maxAmount))
+            {
+                Vector3 pos = x;
+                pos.y = GetTerrainHeightAt(pos, 1000);
+                values.Add(pos);
+            }
+
+            PoolUtility.Free(ref list);
+
+            watch.Stop();
+            onComplete?.Invoke(values, watch.ElapsedMilliseconds);
+        });
+
+        CoroutineUtility.StartCoroutine(task);
+    }
+
+    private static Rect CreateRectAt(Vector3 center, float lenght)
+    {
+        var initial = new Vector2(center.x, center.z);
+        Vector2 topLeftCorner = initial + new Vector2(-lenght, lenght);
+        Vector2 bottomRightCorner = initial + new Vector2(lenght, -lenght);
+
+        var pos = new Vector2(topLeftCorner.x, bottomRightCorner.y);
+        var size = new Vector2(bottomRightCorner.x - topLeftCorner.x, topLeftCorner.y - bottomRightCorner.y);
+        return new Rect(pos, size);
+    }
+
+    private static IEnumerator GetAllAvailableSpawnPointsWithin(Rect rect, TerrainTopology.Enum targetTopology,
+        Action<ConcurrentBag<Vector3>> onComplete)
+    {
+        int xMin = (int)rect.xMin;
+        int xMax = (int)rect.xMax;
+        int yMin = (int)rect.yMin;
+        int yMax = (int)rect.yMax;
+
+        int size = (xMax - xMin) * (yMax - yMin);
+        var list = new ConcurrentBag<Vector3>();
+
+        s_Count = 0;
+        TimeSince timeSince = 0;
+
+        Parallel.For(0, Math.Abs(xMax - xMin), x =>
+        {
+            for (int z = yMin; z < yMax; z++)
+            {
+                Interlocked.Increment(ref s_Count);
+
+                var pos = new Vector3(x + xMin, 0, z);
+                int topology = TerrainMeta.TopologyMap.GetTopology(pos);
+
+                if (!topology.ContainsTopology(targetTopology))
+                    continue;
+
+                list.Add(pos);
+            }
+        });
+
+        while (s_Count < size && timeSince < MAX_DELAY)
+            yield return null;
+
+        Interface.Oxide.NextTick(() => onComplete.Invoke(list)); // NextTick() to bring back on main thread
     }
 }
