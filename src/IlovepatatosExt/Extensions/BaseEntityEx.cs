b11c0380b44adc1b024d8213ff523a988997c70c
Ilovepatatos <gabriel@khome.org>
2024-01-27 15:53:31 -0500

    Create BaseEntityEx.cs

---
+++ src/IlovepatatosExt/Extensions/BaseEntityEx.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class BaseEntityEx
+{
+    public static IEnumerable<T> FilterByTopologyBiome<T>(this IEnumerable<T> values,
+        TerrainTopology.Enum targetTopology, TerrainBiome.Enum targetBiome) where T : BaseEntity
+    {
+        foreach (T entity in values.Where(x => x != null))
+        {
+            Vector3 pos = entity.transform.position;
+
+            int topology = TerrainMeta.TopologyMap.GetTopology(pos);
+            if (!topology.ContainsTopology(targetTopology))
+                continue;
+
+            int biome = TerrainMeta.BiomeMap.GetBiomeMaxType(pos);
+            if ((TerrainBiome.Enum)biome != targetBiome)
+                continue;
+
+            yield return entity;
+        }
+    }
+}
