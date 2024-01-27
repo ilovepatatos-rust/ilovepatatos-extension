3847ad378ed113b002b984fccc350294ca158526
Ilovepatatos <gabriel@khome.org>
2024-01-27 15:53:36 -0500

    Create Prefabs.cs

---
+++ src/IlovepatatosExt/Constants/Prefabs.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace Oxide.Ext.IlovepatatosExt
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public static class Prefabs
+    {
+        public const string GENERIC_MARKER = "assets/prefabs/tools/map/genericradiusmarker.prefab";
+        public const string SPHERE_VISUAL = "assets/prefabs/visualization/sphere.prefab";
+        public const string BATTLE_ROYAL_SHADER = "assets/bundled/prefabs/modding/events/twitch/br_sphere.prefab";
+
+        public const string BED_PREFAB = "assets/prefabs/deployable/bed/bed_deployed.prefab";
+        public const string LOCKER_PREFAB = "assets/prefabs/deployable/locker/locker.deployed.prefab";
+        public const string VENDING_MACHINE = "assets/prefabs/deployable/vendingmachine/vendingmachine.deployed.prefab";
+
+        public const string CODELOCK = "assets/prefabs/locks/keypad/lock.code.prefab";
+        public const string ARCTIC_GARAGE_DOOR_PREFAB = "assets/content/structures/arctic_base_modules/door.hinged.arctic.garage.prefab";
+    }
+}
