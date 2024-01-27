45bda816f32730d1193ebb4f57476b828809e956
Ilovepatatos <gabriel@khome.org>
2024-01-27 15:53:53 -0500

    Create GenericMarker

---
+++ src/IlovepatatosExt/Markers/GenericMarker.cs
@@ -0,0 +1,76 @@
+﻿using JetBrains.Annotations;
+using Network;
+using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public class GenericMarker
+{
+    public MapMarkerGenericRadius Marker;
+
+    public void CreateAt(Vector3 pos, GenericMarkerSettings settings)
+    {
+        CreateAt(pos, settings.Radius, settings.Alpha, settings.InnerColor(), settings.OuterColor());
+    }
+
+    public void CreateAt(Vector3 pos, float radius, float alpha, Color inner, Color outer)
+    {
+        Kill();
+
+        Marker = GameManager.server.CreateEntity(Prefabs.GENERIC_MARKER, pos) as MapMarkerGenericRadius;
+        if (Marker == null) return;
+
+        SetRadius(radius);
+        SetAlpha(alpha);
+        SetColors(inner, outer);
+
+        Marker.enableSaving = false;
+        Marker.Spawn();
+        SendNetworkUpdate();
+    }
+
+    public void Kill()
+    {
+        if (Marker != null)
+            Marker.Kill();
+    }
+
+    public void SetColors(Color inner, Color outer)
+    {
+        if (Marker == null) return;
+
+        Marker.color1 = inner;
+        Marker.color2 = outer;
+    }
+
+    public void SetAlpha(float alpha)
+    {
+        if (Marker != null)
+            Marker.alpha = alpha;
+    }
+
+    public void SetRadius(float radius)
+    {
+        if (Marker != null)
+            Marker.radius = radius / 325f;
+    }
+
+    public void SendNetworkUpdate()
+    {
+        if (Marker != null)
+            Marker.SendUpdate();
+    }
+
+    public void SendAsSnapshotToNewPlayer(BasePlayer player)
+    {
+        if (player == null) return;
+
+        Connection connection = player.Connection;
+        if (connection == null) return;
+
+        Marker.SendAsSnapshot(player.Connection, true);
+        Marker.SendUpdate();
+        Marker.SendUpdateToPlayer(player); // Required to send the colors to the client
+    }
+}
---
+++ src/IlovepatatosExt/Markers/GenericMarkerSettings.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[Serializable]
+public class GenericMarkerSettings
+{
+    public float Radius = 50;
+    public float Alpha = 1;
+    public string HexInnerColor = "#0f961d", HexOuterColor = "#000000";
+
+    public Color InnerColor()
+    {
+        return ColorUtility.ParseHexString(HexInnerColor);
+    }
+
+    public Color OuterColor()
+    {
+        return ColorUtility.ParseHexString(HexOuterColor);
+    }
+}
