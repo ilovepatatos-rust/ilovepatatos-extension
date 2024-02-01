0922a101053d2e32f658376485f23b28047f24d5
Ilovepatatos <gabriel@khome.org>
2024-01-31 19:17:25 -0500

    Create VendingMachineEx.cs

---
+++ src/IlovepatatosExt/Extensions/VendingMachineEx.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace Oxide.Ext.IlovepatatosExt
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public static class VendingMachineEx
+    {
+        public static void SetTitle(this VendingMachine vending, string title)
+        {
+            vending.shopName = title;
+            vending.FullUpdate();
+        }
+    }
+}
