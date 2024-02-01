2326e0eeeb577425d306b062fa528c2af56589e9
Ilovepatatos <gabriel@khome.org>
2024-01-31 19:53:38 -0500

    Create HitInfoEx.cs

---
+++ src/IlovepatatosExt/Extensions/HitInfoEx.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using Rust;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class HitInfoEx
+{
+    public static bool IsSuicide(this HitInfo info)
+    {
+        return info.damageTypes.GetMajorityDamageType() == DamageType.Suicide;
+    }
+}
