2f747e1c098fe68323a55e701bd0876673809ccc
Ilovepatatos <gabriel@khome.org>
2024-01-31 19:51:51 -0500

    Create BaseNetworkableEx.cs

---
+++ src/IlovepatatosExt/Extensions/BaseNetworkableEx.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class BaseNetworkableEx
+{
+    public static void KillOnce(this BaseNetworkable networkable, BaseNetworkable.DestroyMode mode = BaseNetworkable.DestroyMode.None)
+    {
+        if (networkable != null && !networkable.IsDestroyed)
+            networkable.Kill(mode);
+    }
+
+    public static void KillAll(this IEnumerable<BaseNetworkable> networkables, BaseNetworkable.DestroyMode mode = BaseNetworkable.DestroyMode.None)
+    {
+        foreach (BaseNetworkable networkable in networkables)
+            networkable.KillOnce(mode);
+    }
+}
