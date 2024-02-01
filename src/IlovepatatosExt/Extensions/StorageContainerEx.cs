e0544603f649c99c39f44d5f470262c736f6d0ea
Ilovepatatos <gabriel@khome.org>
2024-01-31 20:00:50 -0500

    Create StorageContainerEx.cs

---
+++ src/IlovepatatosExt/Extensions/StorageContainerEx.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class StorageContainerEx
+{
+    public static bool IsFull(this StorageContainer container)
+    {
+        return container.inventory == null || container.inventory.IsFull();
+    }
+}
