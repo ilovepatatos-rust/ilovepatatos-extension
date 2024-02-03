1260ed79ae016b0fc7f48dd3e362db906fefec98
Ilovepatatos <gabriel@khome.org>
2024-02-03 13:19:55 -0500

    Create DataFileSystemEx.cs

---
+++ src/IlovepatatosExt/Extensions/DataFileSystemEx.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using Oxide.Core;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class DataFileSystemEx
+{
+    public static IEnumerable<T> ParseAllFilesAs<T>(this DataFileSystem self)
+    {
+        return self.GetFiles().Select(Path.GetFileNameWithoutExtension).Select(self.ReadObject<T>);
+    }
+}
