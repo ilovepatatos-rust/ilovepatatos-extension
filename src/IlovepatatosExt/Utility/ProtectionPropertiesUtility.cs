35dc86f358fc718505ba3de1eecd17098a63966e
Ilovepatatos <gabriel@khome.org>
2024-02-04 11:32:36 -0500

    Create ProtectionPropertiesUtility.cs

---
+++ src/IlovepatatosExt/Utility/ProtectionPropertiesUtility.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using Rust;
+using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class ProtectionPropertiesUtility
+{
+    public static readonly ProtectionProperties Invincible = CreateProtectionProperties(1f);
+
+    public static ProtectionProperties CreateProtectionProperties(float fallback, params KeyValuePair<DamageType, float>[] protections)
+    {
+        var instance = ScriptableObject.CreateInstance<ProtectionProperties>();
+
+        for (int i = 0; i < instance.amounts.Length; i++)
+            instance.amounts[i] = fallback;
+
+        foreach (KeyValuePair<DamageType, float> value in protections)
+            instance.amounts[(int)value.Key] = value.Value;
+
+        return instance;
+    }
+}
