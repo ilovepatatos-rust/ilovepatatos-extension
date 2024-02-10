68c292653e221c2d229aefcacbbbc22a1453bdfe
Ilovepatatos <gabriel@khome.org>
2024-02-10 00:24:41 -0500

    Create SphereColliderEx.cs

---
+++ src/IlovepatatosExt/Extensions/SphereColliderEx.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+internal static class SphereColliderEx
+{
+    internal static Vector3 GetRandomPointWithinBounds(this SphereCollider col, float offset = 0)
+    {
+        return col.center + Random.insideUnitSphere * (col.radius - offset);
+    }
+}
