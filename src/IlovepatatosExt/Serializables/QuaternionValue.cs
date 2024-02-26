fee727daaeb92ddad463bf0ce94e2dc018cad987
Ilovepatatos <gabriel@khome.org>
2024-02-25 23:53:10 -0500

    Create QuaternionValue.cs

---
+++ src/IlovepatatosExt/Serializables/QuaternionValue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[Serializable]
+public struct QuaternionValue
+{
+    public float X, Y, Z;
+
+    public static implicit operator QuaternionValue(Quaternion value)
+    {
+        Vector3 rot = value.eulerAngles;
+        return new QuaternionValue { X = rot.x, Y = rot.y, Z = rot.z };
+    }
+
+    public static implicit operator Quaternion(QuaternionValue value)
+    {
+        return Quaternion.Euler(value.X, value.Y, value.Z);
+    }
+}
