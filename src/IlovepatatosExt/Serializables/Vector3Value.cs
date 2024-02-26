e137e8d0b24d0871e6aef0055b1b2fbfcfe0a0be
Ilovepatatos <gabriel@khome.org>
2024-02-25 23:53:12 -0500

    Create Vector3Value.cs

---
+++ src/IlovepatatosExt/Serializables/Vector3Value.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[Serializable]
+public struct Vector3Value
+{
+    public float X, Y, Z;
+
+    public static implicit operator Vector3Value(Vector3 value)
+    {
+        return new Vector3Value { X = value.x, Y = value.y, Z = value.z };
+    }
+
+    public static implicit operator Vector3(Vector3Value value)
+    {
+        return new Vector3(value.X, value.Y, value.Z);
+    }
+}
