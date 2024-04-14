137f8a6993b05247975cb97a394cff3ccb1f09b0
Ilovepatatos <gabriel@khome.org>
2024-04-14 15:49:19 -0400

    Create ComponentEx.cs

---
+++ src/IlovepatatosExt/Extensions/ComponentEx.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class ComponentEx
+{
+    public static bool TryDestroyComponent<T>(this Component obj, bool immediate = false) where T : Component
+    {
+        var comp = obj.GetComponent<T>();
+        if (comp == null) return false;
+
+        if (immediate)
+        {
+            UnityEngine.Object.DestroyImmediate(comp);
+        }
+        else
+        {
+            UnityEngine.Object.Destroy(comp);
+        }
+
+        return true;
+    }
+}
