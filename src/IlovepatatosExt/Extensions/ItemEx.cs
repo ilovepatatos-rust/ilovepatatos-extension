6482edeb0d325acf858459773d14234828c6c0b3
Ilovepatatos <gabriel@khome.org>
2024-06-08 12:02:25 -0400

    Create ItemEx.cs

---
+++ src/IlovepatatosExt/Extensions/ItemEx.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace Oxide.Ext.AtlasExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class ItemEx
+{
+    public static void SetSkin(this Item item, ulong skinID)
+    {
+        if (item.skin == skinID)
+            return;
+
+        item.skin = skinID;
+        item.MarkDirty();
+    }
+}
