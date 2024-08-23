4150bdd00408d203337bcf51089c7fa49a1e0d00
Ilovepatatos <gabriel@khome.org>
2024-08-23 01:18:26 -0400

    Update Clock.cs

--- src/IlovepatatosExt/Clock/Clock.cs
+++ src/IlovepatatosExt/Clock/Clock.cs
@@ -6,11 +6,14 @@ namespace Oxide.Ext.IlovepatatosExt;
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public class Clock : Callback
 {
-    public static Clock Create(Plugin owner = null)
+    public static Clock New(Plugin owner = null)
     {
         var clock = PoolUtility.Get<Clock>();
         clock.PluginOwner = owner;
 
         return clock;
     }
+
+    [Obsolete($"Use {nameof(New)} instead.")]
+    public static Clock Create(Plugin owner = null) => New(owner);
 }
