cb267f2096297eacf928fb3ee71f1bee911ec8e6
Ilovepatatos <gabriel@khome.org>
2024-08-27 00:38:40 -0400

    Fix obsolete code

--- src/IlovepatatosExt/Extensions/MapMarkerGenericRadiusEx.cs
+++ src/IlovepatatosExt/Extensions/MapMarkerGenericRadiusEx.cs
@@ -7,13 +7,10 @@ public static class MapMarkerGenericRadiusEx
 {
     public static void SendUpdateToPlayer(this MapMarkerGenericRadius marker, BasePlayer player)
     {
-        float a = marker.color1.a;
-        float alpha = marker.alpha;
-        float radius = marker.radius;
-
         var inner = marker.color1.ToVector3();
         var outer = marker.color2.ToVector3();
 
-        marker.ClientRPCPlayer(null, player, "MarkerUpdate", inner, a, outer, alpha, radius);
+        RpcTarget target = RpcTarget.Player("MarkerUpdate", player);
+        marker.ClientRPC(target, inner, marker.color1.a, outer, marker.alpha, marker.radius);
     }
 }
