1b7b39b04c223a89a1d99aedb6770cf1f2ec94c3
Ilovepatatos <gabriel@khome.org>
2024-09-13 01:31:09 -0400

    Create PlayerProviders.cs

---
+++ src/IlovepatatosExt/Providers/PlayerProviders.cs
@@ -0,0 +1,44 @@
+﻿using Facepunch;
+using JetBrains.Annotations;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public class PlayerProviders : IPlayerProvider, Pool.IPooled
+{
+    private List<IPlayerProvider> _providers;
+
+    public static PlayerProviders New(params IPlayerProvider[] providers)
+    {
+        var provider = PoolUtility.Get<PlayerProviders>();
+        provider._providers.AddRange(providers);
+
+        return provider;
+    }
+
+    public IEnumerable<BasePlayer> GetPlayers()
+    {
+        var done = PoolUtility.Get<HashSet<ulong>>();
+
+        foreach (IPlayerProvider provider in _providers)
+        {
+            foreach (BasePlayer player in provider.GetPlayers())
+            {
+                if (player != null && done.Add(player.userID))
+                    yield return player;
+            }
+        }
+
+        PoolUtility.Free(ref done);
+    }
+
+    void Pool.IPooled.EnterPool()
+    {
+        PoolUtility.Free(ref _providers);
+    }
+
+    void Pool.IPooled.LeavePool()
+    {
+        _providers = PoolUtility.Get<List<IPlayerProvider>>();
+    }
+}
