0b1fee59c35181be799499b47e022da2588290f6
Ilovepatatos <gabriel@khome.org>
2024-12-03 05:39:24 -0500

    Update ObjectEx.cs

--- src/IlovepatatosExt/Extensions/ObjectEx.cs
+++ src/IlovepatatosExt/Extensions/ObjectEx.cs
@@ -5,6 +5,14 @@ namespace Oxide.Ext.IlovepatatosExt;
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public static class ObjectEx
 {
+    public static T To<T>(this object obj)
+    {
+        if (obj is T value)
+            return value;
+
+        return default;
+    }
+
     public static bool ToBool(this object obj)
     {
         if (obj is bool value)
