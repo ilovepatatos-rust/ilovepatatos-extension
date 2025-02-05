49fe7c0317d62a802b5bd0be97f614c883653d91
Ilovepatatos <gabriel@khome.org>
2025-02-04 21:30:05 -0500

    Create ProtoStorageUtility.cs

---
+++ src/IlovepatatosExt/Utility/ProtoStorageUtility.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using Oxide.Core;
+
+namespace Oxide.Ext.IlovepatatosExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class ProtoStorageUtility
+{
+    public static T TryLoad<T>(params string[] subPaths)
+    {
+        return ProtoStorage.Exists(subPaths) ? ProtoStorage.Load<T>(subPaths) : default;
+    }
+}
