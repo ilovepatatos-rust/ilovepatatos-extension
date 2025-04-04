f26068cb8d6242c033fe1a954d1887915d8100a6
Ilovepatatos <gabriel@khome.org>
2025-04-04 04:40:44 -0400

    Update StorageContainer.IsFull()

--- src/IlovepatatosExt/Extensions/StorageContainerEx.cs
+++ src/IlovepatatosExt/Extensions/StorageContainerEx.cs
@@ -5,8 +5,8 @@ namespace Oxide.Ext.IlovepatatosExt;
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public static class StorageContainerEx
 {
-    public static bool IsFull(this StorageContainer container)
+    public static bool IsFull(this StorageContainer container, bool checkForPartialStacks = false)
     {
-        return container.inventory == null || container.inventory.IsFull();
+        return container.inventory == null || container.inventory.IsFull(checkForPartialStacks);
     }
 }
