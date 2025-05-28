16a1277ebb6ba079bc7628744f0296d9f25615c2
Ilovepatatos <gabriel@khome.org>
2025-05-28 04:33:46 -0400

    Rework FormatNoThrow() logic

--- src/IlovepatatosExt/Extensions/StringEx.cs
+++ src/IlovepatatosExt/Extensions/StringEx.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿using System.Text.RegularExpressions;
 using JetBrains.Annotations;
 
 namespace Oxide.Ext.IlovepatatosExt;
@@ -6,6 +6,8 @@ namespace Oxide.Ext.IlovepatatosExt;
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public static class StringEx
 {
+    private static readonly Regex s_placeholderRegex = new(@"\{(\d+)(?:\:([^}]+))?\}", RegexOptions.Compiled);
+
     [MustUseReturnValue]
     public static string FormatNoThrow(this string format, params object[] args)
     {
@@ -18,67 +20,35 @@ public static class StringEx
         }
         catch
         {
-            return ManuallyFormatArgs(format, args);
+            return s_placeholderRegex.Replace(format, match => EvaluatePlaceholder(match, args));
         }
     }
 
-    private static string ManuallyFormatArgs(ReadOnlySpan<char> span, object[] args)
+    private static string EvaluatePlaceholder(Match match, object[] args)
     {
-        var sb = PoolUtility.Get<StringBuilder>();
-        int length = span.Length;
+        Group indexGroup = match.Groups[1];
+        Group formatGroup = match.Groups[2];
 
-        for (int i = 0; i < length; i++)
+        if (!int.TryParse(indexGroup.Value, out int index))
+            return match.Value;
+
+        if (index < 0 || index >= args.Length)
+            return match.Value;
+
+        object arg = args[index];
+        if (arg == null)
+            return string.Empty;
+
+        if (!formatGroup.Success)
+            return arg.ToString();
+
+        try
         {
-            char c = span[i];
-            int start = i + 1;
-
-            switch (c)
-            {
-                case '{' when start < length && span[start] == '{':
-                    sb.Append('{');
-                    i++; // Skip second '{'
-                    continue;
-                case '{':
-                {
-                    int end = start;
-                    while (end < length && span[end] != '}')
-                        end++;
-
-                    if (end >= length)
-                    {
-                        // Unclosed '{', treat as literal
-                        sb.Append('{');
-                        continue;
-                    }
-
-                    ReadOnlySpan<char> indexString = span.Slice(start, end - start);
-                    if (int.TryParse(indexString, out int index) && index >= 0 && index < args.Length)
-                    {
-                        object arg = args[index];
-                        sb.Append(arg?.ToString() ?? string.Empty);
-                    }
-                    else
-                    {
-                        // Leave invalid or missing tokens unchanged
-                        sb.Append('{').Append(indexString).Append('}');
-                    }
-
-                    i = end; // Move past '}'
-                    continue;
-                }
-                case '}' when start < length && span[start] == '}':
-                    sb.Append('}');
-                    i++; // Skip second '}'
-                    continue;
-                default:
-                    sb.Append(c);
-                    break;
-            }
+            return string.Format($"{{0:{formatGroup.Value}}}", arg);
         }
-
-        string text = sb.ToString();
-        PoolUtility.Free(ref sb);
-
-        return text;
+        catch
+        {
+            return arg.ToString();
+        }
     }
 }
